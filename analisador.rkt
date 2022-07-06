8c2ec58f4273dc445c15636310d8b6ac700668c3
Denisse Dominguez Bolaños <80787054+DenisseDB@users.noreply.github.com>
2022-07-06 12:19:49 -0500

    Create analisador.rkt

---
+++ analisador.rkt
@@ -0,0 +1,46 @@
+#lang racket
+(require 2htdp/batch-io)
+
+; Denisse Dominguez Bolaños A01702603
+; ANALISADOR SINTAXICO
+
+; input file
+(define input-filename "test.cpp")
+
+; output file
+(define output-filename "output.cpp")
+
+; html
+(define html "<!DOCTYPE html>
+<html lang=\"en\"><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+<link rel=\"stylesheet\" href=\"./styles.css\">
+<title>Resaltador de Sintaxis</title></head><body>
+<h3 class=\"variable\"/h3>
+<h3 class=\"operadores\" /h3>
+<h3 class=\"numero\"/h3>
+<h3 class=\"especiales\"/h3>
+<h3 class=\"comentarios\"/h3>
+<h3 class=\"reservadas\"/h3>
+</div><br>")
+
+
+; definimos los tokens 
+(define numero #rx"-*[0-9]+\\.[0-9]+([E][-*][0-9]+)?|-*[0-9]+(\\.[0-9]+)?")
+(define variable #rx"[a-zA-Z][a-zA-Z_0-9]*")
+(define operadores #rx"[\\*|\\/|\\^|\\=|\\+|\\-]")
+(define especiales #rx"[\\)\\(\\#\\;\\.\\}\\{\\<\\>]")
+(define comentarios #rx"[\\(\\)]")
+(define reservadas #rx"\\auto|\\else|\\long|\\switch|\\break|\\typedef|\\return|\\char|\\float|\\const|\\for|\\sizeof|\\if|\\while|\\do|\\int|\\double")
+
+
+; usamos regex que nos ayuda a determinar la secuencia
+; usamos match para construir el regx en base a un patron de expresiones regulares especificados ;string
+
+(define (looking el)
+  (cond
+    [(regexp-match? numero el) (string-append "<span class=\"numero\">" el "</span>")]
+    [(regexp-match? variable el) (string-append "<span class=\"variable\">" el "</span>")]
+    [(regexp-match? operadores el) (string-append "<span class=\"operadores\">" el "</span>")]
+    [(regexp-match? especiales el) (string-append "<span class=\"especiales\">" el "</span>")]
+    [(regexp-match? comentarios el) (string-append "<span class=\"comentarios\">" el "</span>")]
+    [(regexp-match? reservadas el) (string-append "<span class=\"reservadas\">" el "</span>")]
